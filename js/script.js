1d6659d4acad371495752f8c38225f3717c7920a
skancher <sohank@gmail.com>
2024-01-08 18:33:40 -0800

    Add javascript code to play a single round

--- js/script.js
+++ js/script.js
@@ -11,3 +11,29 @@ function getComputerChoice() {
     }
 }
 
+// Plays a single round of rock paper scissors
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCaser()
+    if (playerSelection === computerSelection) {
+        return "It's a Tie!"
+    } else if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            return "You Lose! Paper beats Rock";
+        } else {
+            return "You Win! Rock beats Scissors";
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            return "You Lose! Scissors beats Paper";
+        } else {
+            return "You Win! Paper beats Rock";
+        }
+    } else {
+        if (computerSelection === "rock") {
+            return "You Lose! Rock beats Scissors";
+        } else {
+            return "You Win! Scissors beats Paper";
+        }
+    }
+}
+
