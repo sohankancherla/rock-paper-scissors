ac8a6de050314f680b460007419767247ef03c44
skancher <sohank@gmail.com>
2024-01-10 14:25:47 -0800

    Modify old getComputerChoice function

--- js/script.js
+++ js/script.js
@@ -1,14 +1,6 @@
 // Gets the computer's random choice of rock, paper, or scissors
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3) + 1;
-
-    if (computerChoice === 1) {
-        return "rock";
-    } else if (computerChoice === 2) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
+    return Math.floor(Math.random() * 3) + 1;
 }
 
 // Plays a single round of rock paper scissors
