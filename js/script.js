0f076c9e3cba6252dc638d2ff3581c20fabf06d6
skancher <sohank@gmail.com>
2024-01-10 15:10:36 -0800

    Add event listners for rock, paper, and scissors buttons

--- js/script.js
+++ js/script.js
@@ -5,34 +5,36 @@ function getComputerChoice() {
 
 // Plays a single round of rock paper scissors
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase()
     if (playerSelection === computerSelection) {
-        return "It's a Tie!"
-    } else if (playerSelection === "rock") {
-        if (computerSelection === "paper") {
-            return "You Lose! Paper beats Rock";
+        console.log("It's a Tie!");
+    } else if (playerSelection === 1) {
+        if (computerSelection === 2) {
+            console.log("You Lose! Paper beats Rock");
         } else {
-            return "You Win! Rock beats Scissors";
+            console.log("You Win! Rock beats Scissors");
         }
-    } else if (playerSelection === "paper") {
-        if (computerSelection === "scissors") {
-            return "You Lose! Scissors beats Paper";
+    } else if (playerSelection === 2) {
+        if (computerSelection === 3) {
+            console.log("You Lose! Scissors beats Paper");
         } else {
-            return "You Win! Paper beats Rock";
+            console.log("You Win! Paper beats Rock");
         }
     } else {
-        if (computerSelection === "rock") {
-            return "You Lose! Rock beats Scissors";
+        if (computerSelection === 3) {
+            console.log("You Lose! Rock beats Scissors");
         } else {
-            return "You Win! Scissors beats Paper";
+            console.log("You Win! Scissors beats Paper");
         }
     }
 }
 
-
-rockButton = document.querySelector("#rock");
-paperButton = document.querySelector("#paper");
-scissorsButton = document.querySelector("#scissors");
-
 let playerScore = 0;
 let computerScore = 0;
+
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+
+rockButton.addEventListener("click", () => playRound(1, getComputerChoice()))
+paperButton.addEventListener("click", () => playRound(2, getComputerChoice()))
+scissorsButton.addEventListener("click", () => playRound(3, getComputerChoice()))
